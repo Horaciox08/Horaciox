7ee7b1e44202ec8b8b3910a9a9751b1b98ad5be3
Horaciox <horacrak@gmail.com>
2024-08-14 19:20:05 -0400

    V3

--- Codigos/Calculadora.py
+++ Codigos/Calculadora.py
@@ -1,4 +1,11 @@
-nombre= int(input("Ingrese numero"))
-print(nombre)
 
-print(type(nombre))
+
+
+def suma(a,b):
+    resultado = a+b
+def resta(a,b):
+    resultado = a-b
+def multiplicacion(a,b):
+    resultado = a*b
+def division(a,b):
+    resultado = a/b
